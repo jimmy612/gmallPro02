1aabaa91ed15d18c7f91416cc4e194de35e1c9ad
jimmy612 <929210632@qq.com>
2018-08-20 21:07:09 +0800

    upload on 20th Aug

--- gmall-api/src/main/java/com/atguigu/gmall/service/OrderService.java
+++ gmall-api/src/main/java/com/atguigu/gmall/service/OrderService.java
@@ -1,9 +1,21 @@
 package com.atguigu.gmall.service;
 
+import com.atguigu.gmall.bean.OrderInfo;
+
 /**
  * @author Jimmy Hao
  * 2018-08-15 21:05
  */
 public interface OrderService {
     String genTradeCode(String userId);
+
+    boolean checkTradeCode(String tradeCode, String userId);
+
+    String saveOrder(OrderInfo orderInfo);
+
+    OrderInfo getOrderById(String orderId);
+
+    void updateOrderStatus(OrderInfo outTradeNo);
+
+    void sendOrderResultQueue(String outTradeNo);
 }
